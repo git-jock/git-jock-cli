480babc0706665020997216ed095cd604f4837f1
GitHub <github@users.noreply.github.com>
2020-11-27 19:06:59 +0000

    Released 0.2.0b2 to Test PyPI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 setup(
     name='git-jock',
     # This will be updated automatically
-    version='0.2.0b1',
+    version='0.2.0b2',
     author='Gavin Fenton',
     author_email='contact@gavinfenton.com',
     description='Git helper for multi-repository management',
