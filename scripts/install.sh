e883762a5fd0304b2deec422f7e0b2ad40bee169
Gavin Fenton <20977010+GavinF17@users.noreply.github.com>
2020-11-29 14:59:53 +0000

    #58 Added tidy up and echos

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,12 +5,18 @@ ZIP_NAME="$ARCH_NAME.zip"
 TMP_ZIP="/tmp/$ZIP_NAME"
 TMP_UNZIP="/tmp/$ARCH_NAME"
 
+echo "Downloading $ARCH_NAME to tmp"
 rm "$TMP_ZIP"
 curl -L "https://github.com/git-jock/git-jock-cli/releases/latest/download/$ZIP_NAME" --output "$TMP_ZIP"
 
+echo "Unzipping in tmp"
 rm -r "$TMP_UNZIP"
 mkdir "$TMP_UNZIP"
-
 unzip "$TMP_ZIP" -d "$TMP_UNZIP"
 
+echo "Moving jock to /usr/bin"
 mv "$TMP_UNZIP/jock" /usr/bin/
+
+echo "Removing tmp files"
+rm "$TMP_ZIP"
+rm -r "$TMP_UNZIP"
