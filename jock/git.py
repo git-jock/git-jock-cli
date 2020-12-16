9e947600b6fe1e36b39205d3842ba50b29f0fbe2
Gavin Fenton <20977010+GavinF17@users.noreply.github.com>
2020-12-16 22:15:11 +0000

    #113 Fixed test_git

--- jock/git.py
+++ jock/git.py
@@ -19,7 +19,7 @@ def git_clone(selected_repositories, git_args=()):
         repository = selected_repositories[repository_name]
         repository_path = get_repository_path(repository['location'])
         click.echo(
-            'Cloning [{}] in [{}]'.format(repository, repository_path)
+            'Cloning [{}] in [{}]'.format(repository_name, repository_path)
         )
         subprocess.run(('git', 'clone', repository['address'], repository_path) + git_args)
 
@@ -42,7 +42,6 @@ GIT_COMMANDS = {
 
 
 def git_command(command, selected_repositories, git_args):
-    print(selected_repositories)
     git_func = GIT_COMMANDS.get(command)
 
     if git_func is None:
