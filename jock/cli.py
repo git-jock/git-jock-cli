cebb4edbabda1410d82408b046b135b7cae2e6e9
Gavin Fenton <20977010+GavinF17@users.noreply.github.com>
2020-12-16 22:03:29 +0000

    #113 Now working with groups

--- jock/cli.py
+++ jock/cli.py
@@ -25,99 +25,102 @@ CONTEXT_SETTINGS = dict(ignore_unknown_options=True, )
 @click.pass_context
 def main(ctx, repository, group):
     ctx.ensure_object(dict)
-    ctx.obj[CONFIG_REPOSITORIES] = get_selected_repositories(repository, group)
-    ctx.obj[SELECTED_REPOSITORIES] = tuple(map(lambda x: x.lstrip(" ="), repository))
+
+    repositories = tuple(map(lambda x: x.lstrip(" ="), repository))
+    groups = tuple(map(lambda x: x.lstrip(" ="), group))
+
+    ctx.obj[SELECTED_REPOSITORIES] = get_selected_repositories(repositories, groups)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def clone(ctx, git_args):
-    git_command('clone', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('clone', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def add(ctx, git_args):
-    git_command('add', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('add', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def restore(ctx, git_args):
-    git_command('restore', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('restore', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def rm(ctx, git_args):
-    git_command('rm', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('rm', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def branch(ctx, git_args):
-    git_command('branch', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('branch', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def commit(ctx, git_args):
-    git_command('commit', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('commit', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def reset(ctx, git_args):
-    git_command('reset', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('reset', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def switch(ctx, git_args):
-    git_command('switch', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('switch', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def tag(ctx, git_args):
-    git_command('tag', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('tag', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def fetch(ctx, git_args):
-    git_command('fetch', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('fetch', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def pull(ctx, git_args):
-    git_command('pull', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('pull', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def push(ctx, git_args):
-    git_command('push', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('push', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 @main.command(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 @click.argument(GIT_ARGS, nargs=-1, required=False)
 def checkout(ctx, git_args):
-    git_command('checkout', ctx.obj[CONFIG_REPOSITORIES], ctx.obj[SELECTED_REPOSITORIES], git_args)
+    git_command('checkout', ctx.obj[SELECTED_REPOSITORIES], git_args)
 
 
 if __name__ == '__main__':
--- jock/config.py
+++ jock/config.py
@@ -11,18 +11,21 @@ except ImportError:
 
 def load_config():
     with open(os.path.expanduser('~/.jockrc'), 'r') as file:
-        config = yaml.load(file, Loader=Loader)
-        return config
+        return yaml.load(file, Loader=Loader)
 
 
 def get_selected_repositories(selected_repositories, selected_groups):
     config = load_config()
     config_groups = config['groups']
     config_repositories = config['repositories']
-    print(selected_groups)
-    selected_repositories_set = set(selected_repositories)
-    # for group in selected_groups:
-    #     for sub_
-    selected_repository_dict = dict()
-    for selected_repository in selected_repositories_set:
-        selected_repository_dict[selected_repository] = config_repositories[selected_repository]
+
+    repositories = dict({})
+
+    for repo_name in selected_repositories:
+        repositories[repo_name] = config_repositories[repo_name]
+
+    for group_name in selected_groups:
+        for repo_name in config_groups[group_name]['repositories']:
+            repositories[repo_name] = config_repositories[repo_name]
+
+    return repositories
