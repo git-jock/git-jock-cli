2dc379c31cb6d41f8769947b6a0279cebf00a1de
Gavin Fenton <20977010+GavinF17@users.noreply.github.com>
2020-12-17 21:59:06 +0000

    #113 Added config tests

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -4,10 +4,8 @@ from unittest.mock import patch
 
 import yaml
 
-from jock.config import load_config
-from tests.utils import CONFIG_REPOSITORIES
-
-CONFIG = dict({'repositories': CONFIG_REPOSITORIES})
+from jock.config import load_config, get_selected_repositories
+from tests.utils import CONFIG_REPOSITORIES, REPOSITORY_NAMES, GROUP_NAMES, CONFIG_GROUPS
 
 open_name = '%s.open' % __name__
 
@@ -20,12 +18,69 @@ class TestConfig(TestCase):
         # Given
         expected_rc_path = '~/.jockrc'
         expected_expanded_path = '/some/path'
+        expected_config = dict({'repositories': CONFIG_REPOSITORIES})
         mock_expanduser.return_value = expected_expanded_path
-        mock_yaml.return_value = CONFIG
+        mock_yaml.return_value = expected_config
         # When
         actual_config = load_config()
         # Then
         mock_expanduser.assert_called_once_with(expected_rc_path)
         mock_open.assert_called_once_with(expected_expanded_path, 'r')
         mock_yaml.assert_called_once()
-        self.assertEqual(CONFIG, actual_config)
+        self.assertEqual(expected_config, actual_config)
+
+    @patch('jock.config.load_config')
+    def test_get_selected_repositories_returns_selected_repos(self, mock_load_config):
+        # Given
+        selected_repositories = (REPOSITORY_NAMES[0], REPOSITORY_NAMES[2])
+        mock_load_config.return_value = dict({
+            'repositories': CONFIG_REPOSITORIES,
+            'groups': CONFIG_REPOSITORIES
+        })
+        expected_repositories = dict({
+            REPOSITORY_NAMES[0]: CONFIG_REPOSITORIES[REPOSITORY_NAMES[0]],
+            REPOSITORY_NAMES[2]: CONFIG_REPOSITORIES[REPOSITORY_NAMES[2]]
+        })
+        # When
+        actual_repositories = get_selected_repositories(selected_repositories, tuple())
+        # Then
+        mock_load_config.assert_called_once()
+        self.assertEqual(expected_repositories, actual_repositories)
+
+    @patch('jock.config.load_config')
+    def test_get_selected_repositories_returns_selected_groups(self, mock_load_config):
+        # Given
+        selected_groups = (GROUP_NAMES[1], GROUP_NAMES[2])
+        mock_load_config.return_value = dict({
+            'repositories': CONFIG_REPOSITORIES,
+            'groups': CONFIG_GROUPS
+        })
+        expected_repositories = dict({
+            REPOSITORY_NAMES[0]: CONFIG_REPOSITORIES[REPOSITORY_NAMES[0]],
+            REPOSITORY_NAMES[2]: CONFIG_REPOSITORIES[REPOSITORY_NAMES[2]]
+        })
+        # When
+        actual_repositories = get_selected_repositories(tuple(), selected_groups)
+        # Then
+        mock_load_config.assert_called_once()
+        self.assertEqual(expected_repositories, actual_repositories)
+
+    @patch('jock.config.load_config')
+    def test_get_selected_repositories_returns_both_selections(self, mock_load_config):
+        # Given
+        selected_repositories = (REPOSITORY_NAMES[1],)
+        selected_groups = (GROUP_NAMES[1], GROUP_NAMES[2])
+        mock_load_config.return_value = dict({
+            'repositories': CONFIG_REPOSITORIES,
+            'groups': CONFIG_GROUPS
+        })
+        expected_repositories = dict({
+            REPOSITORY_NAMES[0]: CONFIG_REPOSITORIES[REPOSITORY_NAMES[0]],
+            REPOSITORY_NAMES[1]: CONFIG_REPOSITORIES[REPOSITORY_NAMES[1]],
+            REPOSITORY_NAMES[2]: CONFIG_REPOSITORIES[REPOSITORY_NAMES[2]]
+        })
+        # When
+        actual_repositories = get_selected_repositories(selected_repositories, selected_groups)
+        # Then
+        mock_load_config.assert_called_once()
+        self.assertEqual(expected_repositories, actual_repositories)
